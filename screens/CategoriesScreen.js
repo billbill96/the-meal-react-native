37bdc1455a2ea99727e58fd9201aeaf24304ebf4
Supannee Mutitanon <billbillgm@gmail.com>
2020-12-28 20:30:19 +0700

    implement meal reducer

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import { View, 
-        Text, 
-        Button, 
-        FlatList, 
-        TouchableOpacity, 
+import { FlatList, 
         StyleSheet} from 'react-native';
 
 import { HeaderButtons, Item} from 'react-navigation-header-buttons';
